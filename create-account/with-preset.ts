0574b3f62b5c5541426e1672f7e9a56161353b48
Derek Chiang <me@derekchiang.com>
2024-01-03 13:20:31 -0500

    Some improvements

--- create-account/with-preset.ts
+++ create-account/with-preset.ts
@@ -1,16 +1,16 @@
-import "dotenv/config";
-import { createEcdsaKernelAccountClient } from "@kerneljs/presets/zerodev";
-import { Hex, zeroAddress } from "viem";
-import { privateKeyToAccount } from "viem/accounts";
-import { polygonMumbai } from "viem/chains";
+import "dotenv/config"
+import { createEcdsaKernelAccountClient } from "@kerneljs/presets/zerodev"
+import { Hex, zeroAddress } from "viem"
+import { privateKeyToAccount } from "viem/accounts"
+import { polygonMumbai } from "viem/chains"
 
-const zeroDevProjectId = process.env.ZERODEV_PROJECT_ID;
-const privateKey = process.env.PRIVATE_KEY;
-if (!zeroDevProjectId) {
-  throw new Error("ZERODEV_PROJECT_ID is not set");
+const zeroDevProjectId = process.env.ZERODEV_PROJECT_ID
+const privateKey = process.env.PRIVATE_KEY
+if (!zeroDevProjectId || !privateKey) {
+  throw new Error("ZERODEV_PROJECT_ID or PRIVATE_KEY is not set")
 }
 
-const signer = privateKeyToAccount(privateKey as Hex);
+const signer = privateKeyToAccount(privateKey as Hex)
 
 const main = async () => {
   const kernelClient = await createEcdsaKernelAccountClient({
@@ -23,17 +23,17 @@ const main = async () => {
     provider: "STACKUP", // defaults to a recommended provider
     index: BigInt(1), // defaults to 0
     usePaymaster: true, // defaults to true
-  });
+  })
 
-  console.log("My account:", kernelClient.account.address);
+  console.log("My account:", kernelClient.account.address)
 
   const txnHash = await kernelClient.sendTransaction({
     to: zeroAddress,
     value: BigInt(0),
     data: "0x",
-  });
+  })
 
-  console.log("txn hash:", txnHash);
+  console.log("txn hash:", txnHash)
 
   const userOpHash = await kernelClient.sendUserOperation({
     userOperation: {
@@ -43,9 +43,9 @@ const main = async () => {
         data: "0x",
       }),
     },
-  });
+  })
 
-  console.log("userOp hash:", userOpHash);
-};
+  console.log("userOp hash:", userOpHash)
+}
 
-main();
+main()
--- create-account/without-preset.ts
+++ create-account/without-preset.ts
@@ -1,27 +1,32 @@
+import "dotenv/config"
 import {
   createKernelAccount,
   createKernelPaymasterClient,
-} from "@kerneljs/core";
-import { signerToEcdsaValidator } from "@kerneljs/ecdsa-validator";
-import { UserOperation, createSmartAccountClient } from "permissionless";
-import { http, Hex, createPublicClient, zeroAddress } from "viem";
-import { privateKeyToAccount } from "viem/accounts";
-import { polygonMumbai } from "viem/chains";
+} from "@kerneljs/core"
+import { signerToEcdsaValidator } from "@kerneljs/ecdsa-validator"
+import { UserOperation, createSmartAccountClient } from "permissionless"
+import { http, Hex, createPublicClient, zeroAddress } from "viem"
+import { privateKeyToAccount } from "viem/accounts"
+import { polygonMumbai } from "viem/chains"
+
+if (!process.env.BUNDLER_RPC || !process.env.PAYMASTER_RPC || !process.env.PRIVATE_KEY) {
+  throw new Error("BUNDLER_RPC or PAYMASTER_RPC or PRIVATE_KEY is not set")
+}
 
 const publicClient = createPublicClient({
   transport: http(process.env.BUNDLER_RPC),
-});
+})
 
-const signer = privateKeyToAccount(process.env.PRIVATE_KEY as Hex);
+const signer = privateKeyToAccount(process.env.PRIVATE_KEY as Hex)
 
 const main = async () => {
   const ecdsaValidator = await signerToEcdsaValidator(publicClient, {
     signer,
-  });
+  })
 
   const account = await createKernelAccount(publicClient, {
     plugin: ecdsaValidator,
-  });
+  })
 
   const kernelClient = createSmartAccountClient({
     account,
@@ -31,20 +36,22 @@ const main = async () => {
       const kernelPaymaster = createKernelPaymasterClient({
         chain: polygonMumbai,
         transport: http(process.env.PAYMASTER_RPC),
-      });
+      })
       return kernelPaymaster.sponsorUserOperation({
         userOperation,
-      });
+      })
     },
-  });
+  })
+
+  console.log("My account:", kernelClient.account.address)
 
   const txnHash = await kernelClient.sendTransaction({
     to: zeroAddress,
     value: BigInt(0),
     data: "0x",
-  });
+  })
 
-  console.log("txn hash:", txnHash);
+  console.log("txn hash:", txnHash)
 
   const userOpHash = await kernelClient.sendUserOperation({
     userOperation: {
@@ -54,9 +61,9 @@ const main = async () => {
         data: "0x",
       }),
     },
-  });
+  })
 
-  console.log("userOp hash:", userOpHash);
-};
+  console.log("userOp hash:", userOpHash)
+}
 
-main();
+main()
