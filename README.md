9c93e44557b73467008a3a338078b52f07c67277
Sahil Vasava <sahilvasava@gmail.com>
2024-01-03 19:59:59 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Kernel.js Examples
+
+## Setup and Installation
+
+Follow these steps to get this project up and running on your local machine:
+
+1. **Clone the repository**
+
+   Use the following command to clone this repository to your local machine:
+
+   ```bash
+   git clone git@github.com:zerodevapp/kernel.js-examples.git
+   ```
+
+2. **Install dependencies**
+
+   Navigate to the project directory and install the dependencies:
+
+   ```bash
+   cd kernel.js-examples
+   npm install
+   ```
+
+3. **Setup environment variables**
+   Copy the `.env.example` file to `.env` and fill in the required values.
+
+4. **Run the script**
+
+   Run the script using the following command:
+
+   ```bash
+   npx ts-node create-account/with-preset.ts
+   ```
---
+++ create-account/with-preset.ts
@@ -0,0 +1,51 @@
+import "dotenv/config";
+import { createEcdsaKernelAccountClient } from "@kerneljs/presets/zerodev";
+import { Hex, zeroAddress } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { polygonMumbai } from "viem/chains";
+
+const zeroDevProjectId = process.env.ZERODEV_PROJECT_ID;
+const privateKey = process.env.PRIVATE_KEY;
+if (!zeroDevProjectId) {
+  throw new Error("ZERODEV_PROJECT_ID is not set");
+}
+
+const signer = privateKeyToAccount(privateKey as Hex);
+
+const main = async () => {
+  const kernelClient = await createEcdsaKernelAccountClient({
+    // required
+    chain: polygonMumbai,
+    projectId: zeroDevProjectId,
+    signer,
+
+    // optional
+    provider: "ALCHEMY", // defaults to a recommended provider
+    index: BigInt(1), // defaults to 0
+    usePaymaster: true, // defaults to true
+  });
+
+  console.log("My account:", kernelClient.account.address);
+
+  const txnHash = await kernelClient.sendTransaction({
+    to: zeroAddress,
+    value: 0n,
+    data: "0x",
+  });
+
+  console.log("txn hash:", txnHash);
+
+  const userOpHash = await kernelClient.sendUserOperation({
+    userOperation: {
+      callData: await kernelClient.account.encodeCallData({
+        to: zeroAddress,
+        value: 0n,
+        data: "0x",
+      }),
+    },
+  });
+
+  console.log("userOp hash:", userOpHash);
+};
+
+main();
