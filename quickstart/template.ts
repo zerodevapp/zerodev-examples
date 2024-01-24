9fb40f97cb61e5d008fc0b363b29a3f625a1b31d
Derek Chiang <me@derekchiang.com>
2024-01-23 22:14:14 -0500

    Reset quickstart template

--- quickstart/template.ts
+++ quickstart/template.ts
@@ -1,10 +1,5 @@
 import "dotenv/config"
-import { parseAbi, publicActions } from "viem"
-import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
-import { createEcdsaKernelAccountClient } from "@zerodev/presets/zerodev"
-import { polygonMumbai } from "viem/chains"
-import { encodeFunctionData } from "viem"
-import { bundlerActions } from "permissionless"
+import { parseAbi } from "viem"
 
 if (!process.env.ZERODEV_PROJECT_ID) {
   throw new Error("ZERODEV_PROJECT_ID is not set")
@@ -18,49 +13,7 @@ const contractABI = parseAbi([
 ])
 
 const main = async () => {
-  const privateKey = generatePrivateKey()
-  const signer = privateKeyToAccount(privateKey)
-
-  const kernelClient = await createEcdsaKernelAccountClient({
-    chain: polygonMumbai,
-    projectId: process.env.ZERODEV_PROJECT_ID!,
-    signer,
-  })
-
-  const accountAddress = kernelClient.account.address
-  console.log("My account:", accountAddress)
-
-  const userOpHash = await kernelClient.sendUserOperation({
-    userOperation: {
-      callData: await kernelClient.account.encodeCallData({
-        to: contractAddress,
-        value: BigInt(0),
-        data: encodeFunctionData({
-          abi: contractABI,
-          functionName: "mint",
-          args: [accountAddress],
-        })
-      })
-    }
-  })
-  console.log("Submitted UserOp:", userOpHash)
-
-  const bundlerClient = kernelClient.extend(bundlerActions)
-
-  const receipt = await bundlerClient.waitForUserOperationReceipt({
-    hash: userOpHash,
-  })
-  console.log("UserOp confirmed:", receipt.userOpHash)
-
-  const publicClient = kernelClient.extend(publicActions)
-
-  const nftBalance = await publicClient.readContract({
-    address: contractAddress,
-    abi: contractABI,
-    functionName: 'balanceOf',
-    args: [accountAddress],
-  })
-  console.log(`NFT balance: ${nftBalance}`)
+  // Add your code here
 }
 
 main()
