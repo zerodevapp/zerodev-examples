cea655cfd52c73ccb522b5728c9c117c09df994a
Derek Chiang <me@derekchiang.com>
2024-02-02 11:38:07 -0500

    Add tutorial code

---
+++ tutorial/completed.ts
@@ -0,0 +1,70 @@
+import "dotenv/config"
+import { createEcdsaKernelAccountClient } from "@zerodev/presets/zerodev"
+import { encodeFunctionData, parseAbi, publicActions } from "viem"
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
+import { polygonMumbai } from "viem/chains"
+import { bundlerActions } from "permissionless"
+
+if (!process.env.ZERODEV_PROJECT_ID) {
+  throw new Error("ZERODEV_PROJECT_ID is not set")
+}
+
+// The NFT contract we will be interacting with
+const contractAddress = '0x34bE7f35132E97915633BC1fc020364EA5134863'
+const contractABI = parseAbi([
+  'function mint(address _to) public',
+  'function balanceOf(address owner) external view returns (uint256 balance)'
+])
+
+const main = async () => {
+  // Construct a signer
+  const privateKey = generatePrivateKey()
+  const signer = privateKeyToAccount(privateKey)
+
+  // Construct a Kernel account client
+  const kernelClient = await createEcdsaKernelAccountClient({
+    chain: polygonMumbai,
+    projectId: process.env.ZERODEV_PROJECT_ID!,
+    signer,
+  })
+
+  const accountAddress = kernelClient.account.address
+  console.log("My account:", accountAddress)
+
+  // Send a UserOp
+  const userOpHash = await kernelClient.sendUserOperation({
+    userOperation: {
+      callData: await kernelClient.account.encodeCallData({
+        to: contractAddress,
+        value: BigInt(0),
+        data: encodeFunctionData({
+          abi: contractABI,
+          functionName: "mint",
+          args: [accountAddress],
+        })
+      })
+    }
+  })
+  console.log("Submitted UserOp:", userOpHash)
+
+  // Wait for the UserOp to be included on-chain
+  const bundlerClient = kernelClient.extend(bundlerActions)
+
+  const receipt = await bundlerClient.waitForUserOperationReceipt({
+    hash: userOpHash,
+  })
+  console.log("UserOp confirmed:", receipt.userOpHash)
+
+  // Print NFT balance
+  const publicClient = kernelClient.extend(publicActions)
+
+  const nftBalance = await publicClient.readContract({
+    address: contractAddress,
+    abi: contractABI,
+    functionName: 'balanceOf',
+    args: [accountAddress],
+  })
+  console.log(`NFT balance: ${nftBalance}`)
+}
+
+main()
---
+++ tutorial/template.ts
@@ -0,0 +1,19 @@
+import "dotenv/config"
+import { parseAbi } from "viem"
+
+if (!process.env.ZERODEV_PROJECT_ID) {
+  throw new Error("ZERODEV_PROJECT_ID is not set")
+}
+
+// The NFT contract we will be interacting with
+const contractAddress = '0x34bE7f35132E97915633BC1fc020364EA5134863'
+const contractABI = parseAbi([
+  'function mint(address _to) public',
+  'function balanceOf(address owner) external view returns (uint256 balance)'
+])
+
+const main = async () => {
+  // Add your code here
+}
+
+main()
