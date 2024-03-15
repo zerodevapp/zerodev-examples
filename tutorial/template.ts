5e3acb3639caec2cc745ebea4e753a7784e9fcb6
Derek Chiang <me@derekchiang.com>
2024-03-15 18:20:59 -0400

    Update tutorial code

--- tutorial/template.ts
+++ tutorial/template.ts
@@ -1,9 +1,17 @@
 import "dotenv/config"
-import { parseAbi } from "viem"
+import { createPublicClient, encodeFunctionData, http, parseAbi, publicActions } from "viem"
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
+import { polygonMumbai } from "viem/chains"
+import { bundlerActions } from "permissionless"
+import { signerToEcdsaValidator } from "@zerodev/ecdsa-validator"
+import { createKernelAccount, createKernelAccountClient, createZeroDevPaymasterClient } from "@zerodev/sdk"
 
 if (!process.env.ZERODEV_PROJECT_ID) {
   throw new Error("ZERODEV_PROJECT_ID is not set")
 }
+
+const BUNDLER_RPC = `https://rpc.zerodev.app/api/v2/bundler/${process.env.ZERODEV_PROJECT_ID}`
+const PAYMASTER_RPC = `https://rpc.zerodev.app/api/v2/paymaster/${process.env.ZERODEV_PROJECT_ID}`
 
 // The NFT contract we will be interacting with
 const contractAddress = '0x34bE7f35132E97915633BC1fc020364EA5134863'
@@ -12,8 +20,15 @@ const contractABI = parseAbi([
   'function balanceOf(address owner) external view returns (uint256 balance)'
 ])
 
+// Construct a public client
+const publicClient = createPublicClient({
+  transport: http(BUNDLER_RPC),
+})
+
 const main = async () => {
-  // Add your code here
+
+  // your code goes here
+
 }
 
 main()
