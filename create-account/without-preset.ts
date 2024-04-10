682a3dfbae62eaa03834df5fdb41feba9899a6e4
Derek Chiang <me@derekchiang.com>
2024-04-09 23:23:39 -0400

    Remove policies

--- create-account/without-preset.ts
+++ create-account/without-preset.ts
@@ -1,61 +1,50 @@
-import "dotenv/config";
+import "dotenv/config"
 import {
   createKernelAccount,
   createZeroDevPaymasterClient,
   createKernelAccountClient,
-} from "@zerodev/sdk";
-import { signerToEcdsaValidator } from "@zerodev/ecdsa-validator";
-import { toPermissionValidator } from "@zerodev/permissions";
-import { toECDSASigner } from "@zerodev/permissions/signers";
-import { toGasPolicy } from "@zerodev/permissions/policies";
-import { ENTRYPOINT_ADDRESS_V07 } from "permissionless";
-import { http, Hex, createPublicClient, zeroAddress, parseEther } from "viem";
-import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
-import { sepolia } from "viem/chains";
+} from "@zerodev/sdk"
+import { signerToEcdsaValidator } from "@zerodev/ecdsa-validator"
+import { toPermissionValidator } from "@zerodev/permissions"
+import { toECDSASigner } from "@zerodev/permissions/signers"
+import { ENTRYPOINT_ADDRESS_V07, bundlerActions } from "permissionless"
+import { http, Hex, createPublicClient, zeroAddress, parseEther } from "viem"
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
+import { sepolia } from "viem/chains"
 
 if (
   !process.env.BUNDLER_RPC ||
   !process.env.PAYMASTER_RPC ||
   !process.env.PRIVATE_KEY
 ) {
-  throw new Error("BUNDLER_RPC or PAYMASTER_RPC or PRIVATE_KEY is not set");
+  throw new Error("BUNDLER_RPC or PAYMASTER_RPC or PRIVATE_KEY is not set")
 }
 
 const publicClient = createPublicClient({
   transport: http(process.env.BUNDLER_RPC),
-});
+})
 
-const signer = privateKeyToAccount(process.env.PRIVATE_KEY as Hex);
-const chain = sepolia;
-const entryPoint = ENTRYPOINT_ADDRESS_V07;
+const signer = privateKeyToAccount(process.env.PRIVATE_KEY as Hex)
+const chain = sepolia
+const entryPoint = ENTRYPOINT_ADDRESS_V07
 
 const main = async () => {
   const ecdsaValidator = await signerToEcdsaValidator(publicClient, {
     signer,
     entryPoint,
-  });
+  })
 
   const ecdsaSigner = toECDSASigner({
     signer: privateKeyToAccount(generatePrivateKey()),
-  });
+  })
 
   const account = await createKernelAccount(publicClient, {
     plugins: {
       sudo: ecdsaValidator,
-      regular: await toPermissionValidator(publicClient, {
-        entryPoint,
-        signer: ecdsaSigner,
-        policies: [
-          await toGasPolicy({
-            allowed: BigInt(parseEther("1")),
-            enforcePaymaster: true,
-          }),
-        ],
-      }),
     },
     entryPoint,
-  });
-  console.log("My account:", account.address);
+  })
+  console.log("My account:", account.address)
 
   const kernelClient = createKernelAccountClient({
     account,
@@ -68,14 +57,14 @@ const main = async () => {
           chain,
           transport: http(process.env.PAYMASTER_RPC),
           entryPoint,
-        });
+        })
         return paymasterClient.sponsorUserOperation({
           userOperation,
           entryPoint,
-        });
+        })
       },
     },
-  });
+  })
 
   const userOpHash = await kernelClient.sendUserOperation({
     userOperation: {
@@ -85,9 +74,16 @@ const main = async () => {
         data: "0x",
       }),
     },
-  });
+  })
 
-  console.log("userOp hash:", userOpHash);
-};
+  console.log("userOp hash:", userOpHash)
 
-main();
+  const bundlerClient = kernelClient.extend(bundlerActions(entryPoint))
+  const _receipt = await bundlerClient.waitForUserOperationReceipt({
+    hash: userOpHash,
+  })
+
+  console.log("userOp completed")
+}
+
+main()
