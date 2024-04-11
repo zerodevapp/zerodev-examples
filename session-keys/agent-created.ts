92ec8e1d72995d0f2a5f367b028feddb799ad2ee
Derek Chiang <me@derekchiang.com>
2024-04-11 13:01:23 -0400

    Rename scripts

--- session-keys/agent-created.ts
+++ session-keys/agent-created.ts
@@ -1,66 +1,69 @@
-import "dotenv/config";
+import "dotenv/config"
 import {
   createKernelAccount,
   createZeroDevPaymasterClient,
   createKernelAccountClient,
-} from "@zerodev/sdk";
-import { signerToEcdsaValidator } from "@zerodev/ecdsa-validator";
-import { ENTRYPOINT_ADDRESS_V07 } from "permissionless";
+  addressToEmptyAccount,
+} from "@zerodev/sdk"
+import { signerToEcdsaValidator } from "@zerodev/ecdsa-validator"
+import { ENTRYPOINT_ADDRESS_V07 } from "permissionless"
 import {
   http,
   Hex,
   createPublicClient,
   parseAbi,
   encodeFunctionData,
-} from "viem";
-import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
-import { sepolia } from "viem/chains";
-import { toECDSASigner } from "@zerodev/permissions/signers";
+  Address,
+} from "viem"
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
+import { sepolia } from "viem/chains"
+import { toECDSASigner } from "@zerodev/permissions/signers"
+import { ParamCondition, toCallPolicy } from "@zerodev/permissions/policies"
 import {
   ModularSigner,
   deserializePermissionAccount,
   serializePermissionAccount,
   toPermissionValidator,
-} from "@zerodev/permissions";
-import { ParamCondition, toCallPolicy } from "@zerodev/permissions/policies";
+} from "@zerodev/permissions"
 
 if (
   !process.env.BUNDLER_RPC ||
   !process.env.PAYMASTER_RPC ||
   !process.env.PRIVATE_KEY
 ) {
-  throw new Error("BUNDLER_RPC or PAYMASTER_RPC or PRIVATE_KEY is not set");
+  throw new Error("BUNDLER_RPC or PAYMASTER_RPC or PRIVATE_KEY is not set")
 }
 
 const publicClient = createPublicClient({
-  chain: sepolia,
   transport: http(process.env.BUNDLER_RPC),
-});
+})
 
-const signer = privateKeyToAccount(process.env.PRIVATE_KEY as Hex);
-const contractAddress = "0x34bE7f35132E97915633BC1fc020364EA5134863";
+const signer = privateKeyToAccount(process.env.PRIVATE_KEY as Hex)
+const contractAddress = "0x34bE7f35132E97915633BC1fc020364EA5134863"
 const contractABI = parseAbi([
   "function mint(address _to) public",
   "function balanceOf(address owner) external view returns (uint256 balance)",
-]);
+])
+const entryPoint = ENTRYPOINT_ADDRESS_V07
 
-const entryPoint = ENTRYPOINT_ADDRESS_V07;
-const createSessionKey = async (
-  sessionKeySigner: ModularSigner,
-  sessionPrivateKey: Hex
-) => {
+const createSessionKey = async (sessionKeyAddress: Address) => {
   const ecdsaValidator = await signerToEcdsaValidator(publicClient, {
     entryPoint,
     signer,
-  });
+  })
 
   const masterAccount = await createKernelAccount(publicClient, {
     entryPoint,
     plugins: {
       sudo: ecdsaValidator,
     },
-  });
-  console.log("Account address:", masterAccount.address);
+  })
+  console.log("Account address:", masterAccount.address)
+
+  // Create an "empty account" as the signer -- you only need the public
+  // key (address) to do this.
+  const emptyAccount = addressToEmptyAccount(sessionKeyAddress)
+  const emptySessionKeySigner = await toECDSASigner({ signer: emptyAccount })
 
   const callPolicy = toCallPolicy({
     permissions: [
@@ -85,13 +88,13 @@ const createSessionKey = async (
         ],
       },
     ],
-  });
+  })
 
   const permissionPlugin = await toPermissionValidator(publicClient, {
     entryPoint,
-    signer: sessionKeySigner,
+    signer: emptySessionKeySigner,
     policies: [callPolicy],
-  });
+  })
 
   const sessionKeyAccount = await createKernelAccount(publicClient, {
     entryPoint,
@@ -99,24 +102,27 @@ const createSessionKey = async (
       sudo: ecdsaValidator,
       regular: permissionPlugin,
     },
-  });
+  })
 
-  // Include the private key when you serialize the session key
-  return await serializePermissionAccount(sessionKeyAccount, sessionPrivateKey);
-};
+  return await serializePermissionAccount(sessionKeyAccount)
+}
 
-const useSessionKey = async (serializedSessionKey: string) => {
+const useSessionKey = async (
+  serializedSessionKey: string,
+  sessionKeySigner: ModularSigner
+) => {
   const sessionKeyAccount = await deserializePermissionAccount(
     publicClient,
     entryPoint,
-    serializedSessionKey
-  );
+    serializedSessionKey,
+    sessionKeySigner
+  )
 
   const kernelPaymaster = createZeroDevPaymasterClient({
     entryPoint,
     chain: sepolia,
     transport: http(process.env.PAYMASTER_RPC),
-  });
+  })
   const kernelClient = createKernelAccountClient({
     entryPoint,
     account: sessionKeyAccount,
@@ -125,7 +131,7 @@ const useSessionKey = async (serializedSessionKey: string) => {
     middleware: {
       sponsorUserOperation: kernelPaymaster.sponsorUserOperation,
     },
-  });
+  })
 
   const userOpHash = await kernelClient.sendUserOperation({
     userOperation: {
@@ -139,26 +145,29 @@ const useSessionKey = async (serializedSessionKey: string) => {
         }),
       }),
     },
-  });
+  })
 
-  console.log("userOp hash:", userOpHash);
-};
+  console.log("userOp hash:", userOpHash)
+}
 
 const main = async () => {
-  const sessionPrivateKey = generatePrivateKey();
-  const sessionKeyAccount = privateKeyToAccount(sessionPrivateKey);
+  // The agent creates a public-private key pair and sends
+  // the public key (address) to the owner.
+  const sessionPrivateKey = generatePrivateKey()
+  const sessionKeyAccount = privateKeyToAccount(sessionPrivateKey)
+
   const sessionKeySigner = await toECDSASigner({
     signer: sessionKeyAccount,
-  });
+  })
 
-  // The owner creates a session key, serializes it, and shares it with the agent.
+  // The owner authorizes the public key by signing it and sending
+  // back the signature
   const serializedSessionKey = await createSessionKey(
-    sessionKeySigner,
-    sessionPrivateKey
-  );
+    sessionKeySigner.account.address
+  )
 
-  // The agent reconstructs the session key using the serialized value
-  await useSessionKey(serializedSessionKey);
-};
+  // The agent constructs a full session key
+  await useSessionKey(serializedSessionKey, sessionKeySigner)
+}
 
-main();
+main()
--- session-keys/owner-created.ts
+++ session-keys/owner-created.ts
@@ -1,69 +1,66 @@
-import "dotenv/config";
+import "dotenv/config"
 import {
   createKernelAccount,
   createZeroDevPaymasterClient,
   createKernelAccountClient,
-  addressToEmptyAccount,
-} from "@zerodev/sdk";
-import { signerToEcdsaValidator } from "@zerodev/ecdsa-validator";
-import { ENTRYPOINT_ADDRESS_V07 } from "permissionless";
+} from "@zerodev/sdk"
+import { signerToEcdsaValidator } from "@zerodev/ecdsa-validator"
+import { ENTRYPOINT_ADDRESS_V07 } from "permissionless"
 import {
   http,
   Hex,
   createPublicClient,
   parseAbi,
   encodeFunctionData,
-  Address,
-} from "viem";
-import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
-import { sepolia } from "viem/chains";
-import { toECDSASigner } from "@zerodev/permissions/signers";
-import { ParamCondition, toCallPolicy } from "@zerodev/permissions/policies";
+} from "viem"
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
+import { sepolia } from "viem/chains"
+import { toECDSASigner } from "@zerodev/permissions/signers"
 import {
   ModularSigner,
   deserializePermissionAccount,
   serializePermissionAccount,
   toPermissionValidator,
-} from "@zerodev/permissions";
+} from "@zerodev/permissions"
+import { ParamCondition, toCallPolicy } from "@zerodev/permissions/policies"
 
 if (
   !process.env.BUNDLER_RPC ||
   !process.env.PAYMASTER_RPC ||
   !process.env.PRIVATE_KEY
 ) {
-  throw new Error("BUNDLER_RPC or PAYMASTER_RPC or PRIVATE_KEY is not set");
+  throw new Error("BUNDLER_RPC or PAYMASTER_RPC or PRIVATE_KEY is not set")
 }
 
 const publicClient = createPublicClient({
+  chain: sepolia,
   transport: http(process.env.BUNDLER_RPC),
-});
+})
 
-const signer = privateKeyToAccount(process.env.PRIVATE_KEY as Hex);
-const contractAddress = "0x34bE7f35132E97915633BC1fc020364EA5134863";
+const signer = privateKeyToAccount(process.env.PRIVATE_KEY as Hex)
+const contractAddress = "0x34bE7f35132E97915633BC1fc020364EA5134863"
 const contractABI = parseAbi([
   "function mint(address _to) public",
   "function balanceOf(address owner) external view returns (uint256 balance)",
-]);
-const entryPoint = ENTRYPOINT_ADDRESS_V07;
+])
 
-const createSessionKey = async (sessionKeyAddress: Address) => {
+const entryPoint = ENTRYPOINT_ADDRESS_V07
+const createSessionKey = async (
+  sessionKeySigner: ModularSigner,
+  sessionPrivateKey: Hex
+) => {
   const ecdsaValidator = await signerToEcdsaValidator(publicClient, {
     entryPoint,
     signer,
-  });
+  })
 
   const masterAccount = await createKernelAccount(publicClient, {
     entryPoint,
     plugins: {
       sudo: ecdsaValidator,
     },
-  });
-  console.log("Account address:", masterAccount.address);
-
-  // Create an "empty account" as the signer -- you only need the public
-  // key (address) to do this.
-  const emptyAccount = addressToEmptyAccount(sessionKeyAddress);
-  const emptySessionKeySigner = await toECDSASigner({ signer: emptyAccount });
+  })
+  console.log("Account address:", masterAccount.address)
 
   const callPolicy = toCallPolicy({
     permissions: [
@@ -88,13 +85,13 @@ const createSessionKey = async (sessionKeyAddress: Address) => {
         ],
       },
     ],
-  });
+  })
 
   const permissionPlugin = await toPermissionValidator(publicClient, {
     entryPoint,
-    signer: emptySessionKeySigner,
+    signer: sessionKeySigner,
     policies: [callPolicy],
-  });
+  })
 
   const sessionKeyAccount = await createKernelAccount(publicClient, {
     entryPoint,
@@ -102,27 +99,24 @@ const createSessionKey = async (sessionKeyAddress: Address) => {
       sudo: ecdsaValidator,
       regular: permissionPlugin,
     },
-  });
+  })
 
-  return await serializePermissionAccount(sessionKeyAccount);
-};
+  // Include the private key when you serialize the session key
+  return await serializePermissionAccount(sessionKeyAccount, sessionPrivateKey)
+}
 
-const useSessionKey = async (
-  serializedSessionKey: string,
-  sessionKeySigner: ModularSigner
-) => {
+const useSessionKey = async (serializedSessionKey: string) => {
   const sessionKeyAccount = await deserializePermissionAccount(
     publicClient,
     entryPoint,
-    serializedSessionKey,
-    sessionKeySigner
-  );
+    serializedSessionKey
+  )
 
   const kernelPaymaster = createZeroDevPaymasterClient({
     entryPoint,
     chain: sepolia,
     transport: http(process.env.PAYMASTER_RPC),
-  });
+  })
   const kernelClient = createKernelAccountClient({
     entryPoint,
     account: sessionKeyAccount,
@@ -131,7 +125,7 @@ const useSessionKey = async (
     middleware: {
       sponsorUserOperation: kernelPaymaster.sponsorUserOperation,
     },
-  });
+  })
 
   const userOpHash = await kernelClient.sendUserOperation({
     userOperation: {
@@ -145,29 +139,26 @@ const useSessionKey = async (
         }),
       }),
     },
-  });
+  })
 
-  console.log("userOp hash:", userOpHash);
-};
+  console.log("userOp hash:", userOpHash)
+}
 
 const main = async () => {
-  // The agent creates a public-private key pair and sends
-  // the public key (address) to the owner.
-  const sessionPrivateKey = generatePrivateKey();
-  const sessionKeyAccount = privateKeyToAccount(sessionPrivateKey);
-
+  const sessionPrivateKey = generatePrivateKey()
+  const sessionKeyAccount = privateKeyToAccount(sessionPrivateKey)
   const sessionKeySigner = await toECDSASigner({
     signer: sessionKeyAccount,
-  });
+  })
 
-  // The owner authorizes the public key by signing it and sending
-  // back the signature
+  // The owner creates a session key, serializes it, and shares it with the agent.
   const serializedSessionKey = await createSessionKey(
-    sessionKeySigner.account.address
-  );
+    sessionKeySigner,
+    sessionPrivateKey
+  )
 
-  // The agent constructs a full session key
-  await useSessionKey(serializedSessionKey, sessionKeySigner);
-};
+  // The agent reconstructs the session key using the serialized value
+  await useSessionKey(serializedSessionKey)
+}
 
-main();
+main()
