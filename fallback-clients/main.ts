0d036a18fda5150407c24add2faf9192a248fe99
Derek Chiang <me@derekchiang.com>
2024-04-25 18:59:27 -0400

    Reorg code

--- fallback-clients/main.ts
+++ fallback-clients/main.ts
@@ -9,85 +9,83 @@ import { sepolia } from 'viem/chains'
 const zeroDevProjectId = process.env.ZERODEV_PROJECT_ID
 const privateKey = process.env.PRIVATE_KEY
 if (!zeroDevProjectId || !privateKey) {
-    throw new Error("ZERODEV_PROJECT_ID or PRIVATE_KEY is not set")
+  throw new Error("ZERODEV_PROJECT_ID or PRIVATE_KEY is not set")
 }
 
 const signer = privateKeyToAccount(privateKey as Hex)
 const chain = sepolia
 const publicClient = createPublicClient({
-    transport: http(process.env.BUNDLER_RPC),
+  transport: http(process.env.BUNDLER_RPC),
 })
 const entryPoint = ENTRYPOINT_ADDRESS_V07
 
 async function main() {
-    const ecdsaValidator = await signerToEcdsaValidator(publicClient, {
-        signer,
-        entryPoint
-    })
+  const ecdsaValidator = await signerToEcdsaValidator(publicClient, {
+    signer,
+    entryPoint
+  })
 
-    const account = await createKernelAccount(publicClient, {
-        plugins: {
-            sudo: ecdsaValidator,
-        },
-        entryPoint
-    })
+  const account = await createKernelAccount(publicClient, {
+    plugins: {
+      sudo: ecdsaValidator,
+    },
+    entryPoint
+  })
 
-    const pimlicoPaymasterClient = createZeroDevPaymasterClient({
-        chain,
-        transport: http(process.env.PAYMASTER_RPC + '?provider=PIMLICO'),
-        entryPoint
-    })
+  const pimlicoKernelClient = createKernelAccountClient({
+    account,
+    chain,
+    bundlerTransport: http(process.env.BUNDLER_RPC + '?provider=PIMLICO'),
+    middleware: {
+      sponsorUserOperation: async ({ userOperation }) => {
+        const pimlicoPaymasterClient = createZeroDevPaymasterClient({
+          chain,
+          transport: http(process.env.PAYMASTER_RPC + '?provider=PIMLICO'),
+          entryPoint
+        })
+        return pimlicoPaymasterClient.sponsorUserOperation({
+          userOperation,
+          entryPoint,
+        })
+      }
+    },
+    entryPoint
+  })
 
-    const stackupPaymasterClient = createZeroDevPaymasterClient({
-        chain,
-        transport: http(process.env.PAYMASTER_RPC + '?provider=STACKUP'),
-        entryPoint
-    })
+  const stackupKernelClient = createKernelAccountClient({
+    account,
+    chain,
+    bundlerTransport: http(process.env.BUNDLER_RPC + '?provider=STACKUP'),
+    middleware: {
+      sponsorUserOperation: async ({ userOperation }) => {
+        const stackupPaymasterClient = createZeroDevPaymasterClient({
+          chain,
+          transport: http(process.env.PAYMASTER_RPC + '?provider=STACKUP'),
+          entryPoint
+        })
+        return stackupPaymasterClient.sponsorUserOperation({
+          userOperation,
+          entryPoint,
+        })
+      }
+    },
+    entryPoint
+  })
 
-    const pimlicoKernelClient = createKernelAccountClient({
-        account,
-        chain,
-        bundlerTransport: http(process.env.BUNDLER_RPC + '?provider=PIMLICO'),
-        middleware: {
-            sponsorUserOperation: async ({ userOperation }) => {
-                return pimlicoPaymasterClient.sponsorUserOperation({
-                    userOperation,
-                    entryPoint,
-                })
-            }
-        },
-        entryPoint
-    })
+  const fallbackKernelClient = createFallbackKernelAccountClient([
+    pimlicoKernelClient,
+    stackupKernelClient
+  ])
 
-    const stackupKernelClient = createKernelAccountClient({
-        account,
-        chain,
-        bundlerTransport: http(process.env.BUNDLER_RPC + '?provider=STACKUP'),
-        middleware: {
-            sponsorUserOperation: async ({ userOperation }) => {
-                return stackupPaymasterClient.sponsorUserOperation({
-                    userOperation,
-                    entryPoint,
-                })
-            }
-        },
-        entryPoint
-    })
+  console.log("Account address:", fallbackKernelClient.account.address)
 
-    const fallbackKernelClient = createFallbackKernelAccountClient([
-        pimlicoKernelClient,
-        stackupKernelClient
-    ])
+  const txHash = await fallbackKernelClient.sendTransaction({
+    to: zeroAddress,
+    value: BigInt(0),
+    data: "0x"
+  })
 
-    console.log("Account address:", fallbackKernelClient.account.address)
-
-    const txHash = await fallbackKernelClient.sendTransaction({
-        to: zeroAddress,
-        value: BigInt(0),
-        data: "0x"
-    })
-
-    console.log("Txn hash:", txHash)
+  console.log("Txn hash:", txHash)
 }
 
 main()
