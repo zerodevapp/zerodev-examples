bce0c135cf8ea510490a8e2a3d80443314583aac
Sahil Vasava <sahilvasava@gmail.com>
2024-06-12 07:00:57 +0530

    feat: add revoke session key example

--- session-keys/transaction-automation.ts
+++ session-keys/transaction-automation.ts
@@ -1,54 +1,49 @@
-import "dotenv/config"
+import "dotenv/config";
 import {
   createKernelAccount,
   createZeroDevPaymasterClient,
   createKernelAccountClient,
   addressToEmptyAccount,
-} from "@zerodev/sdk"
-import { signerToEcdsaValidator } from "@zerodev/ecdsa-validator"
-import { ENTRYPOINT_ADDRESS_V07 } from "permissionless"
-import {
-  http,
-  Hex,
-  createPublicClient, Address,
-  zeroAddress
-} from "viem"
-import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
-import { sepolia } from "viem/chains"
-import { toECDSASigner } from "@zerodev/permissions/signers"
-import { toSudoPolicy } from "@zerodev/permissions/policies"
+} from "@zerodev/sdk";
+import { signerToEcdsaValidator } from "@zerodev/ecdsa-validator";
+import { ENTRYPOINT_ADDRESS_V07, bundlerActions } from "permissionless";
+import { http, Hex, createPublicClient, Address, zeroAddress } from "viem";
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
+import { sepolia } from "viem/chains";
+import { toECDSASigner } from "@zerodev/permissions/signers";
+import { toSudoPolicy } from "@zerodev/permissions/policies";
 import {
   ModularSigner,
   deserializePermissionAccount,
   serializePermissionAccount,
   toPermissionValidator,
-} from "@zerodev/permissions"
+} from "@zerodev/permissions";
 
 if (
   !process.env.BUNDLER_RPC ||
   !process.env.PAYMASTER_RPC ||
   !process.env.PRIVATE_KEY
 ) {
-  throw new Error("BUNDLER_RPC or PAYMASTER_RPC or PRIVATE_KEY is not set")
+  throw new Error("BUNDLER_RPC or PAYMASTER_RPC or PRIVATE_KEY is not set");
 }
 
 const publicClient = createPublicClient({
   transport: http(process.env.BUNDLER_RPC),
-})
+});
 
-const signer = privateKeyToAccount(process.env.PRIVATE_KEY as Hex)
-const entryPoint = ENTRYPOINT_ADDRESS_V07
+const signer = privateKeyToAccount(process.env.PRIVATE_KEY as Hex);
+const entryPoint = ENTRYPOINT_ADDRESS_V07;
 
 const getApproval = async (sessionKeyAddress: Address) => {
   const ecdsaValidator = await signerToEcdsaValidator(publicClient, {
     entryPoint,
     signer,
-  })
+  });
 
   // Create an "empty account" as the signer -- you only need the public
   // key (address) to do this.
-  const emptyAccount = addressToEmptyAccount(sessionKeyAddress)
-  const emptySessionKeySigner = await toECDSASigner({ signer: emptyAccount })
+  const emptyAccount = addressToEmptyAccount(sessionKeyAddress);
+  const emptySessionKeySigner = await toECDSASigner({ signer: emptyAccount });
 
   const permissionPlugin = await toPermissionValidator(publicClient, {
     entryPoint,
@@ -58,7 +53,7 @@ const getApproval = async (sessionKeyAddress: Address) => {
       // In practice, you would want to set more restrictive policies.
       toSudoPolicy({}),
     ],
-  })
+  });
 
   const sessionKeyAccount = await createKernelAccount(publicClient, {
     entryPoint,
@@ -66,10 +61,10 @@ const getApproval = async (sessionKeyAddress: Address) => {
       sudo: ecdsaValidator,
       regular: permissionPlugin,
     },
-  })
+  });
 
-  return await serializePermissionAccount(sessionKeyAccount)
-}
+  return await serializePermissionAccount(sessionKeyAccount);
+};
 
 const useSessionKey = async (
   approval: string,
@@ -80,13 +75,13 @@ const useSessionKey = async (
     entryPoint,
     approval,
     sessionKeySigner
-  )
+  );
 
   const kernelPaymaster = createZeroDevPaymasterClient({
     entryPoint,
     chain: sepolia,
     transport: http(process.env.PAYMASTER_RPC),
-  })
+  });
   const kernelClient = createKernelAccountClient({
     entryPoint,
     account: sessionKeyAccount,
@@ -95,7 +90,7 @@ const useSessionKey = async (
     middleware: {
       sponsorUserOperation: kernelPaymaster.sponsorUserOperation,
     },
-  })
+  });
 
   const userOpHash = await kernelClient.sendUserOperation({
     userOperation: {
@@ -105,27 +100,82 @@ const useSessionKey = async (
         data: "0x",
       }),
     },
-  })
+  });
 
-  console.log("userOp hash:", userOpHash)
-}
+  console.log("userOp hash:", userOpHash);
+
+  const bundlerClient = kernelClient.extend(bundlerActions(entryPoint));
+  const _receipt = await bundlerClient.waitForUserOperationReceipt({
+    hash: userOpHash,
+  });
+  console.log({ txHash: _receipt.receipt.transactionHash });
+};
+
+const revokeSessionKey = async (sessionKeyAddress: Address) => {
+  const ecdsaValidator = await signerToEcdsaValidator(publicClient, {
+    entryPoint,
+    signer,
+  });
+  const sudoAccount = await createKernelAccount(publicClient, {
+    plugins: {
+      sudo: ecdsaValidator,
+    },
+    entryPoint,
+  });
+
+  const kernelPaymaster = createZeroDevPaymasterClient({
+    entryPoint,
+    chain: sepolia,
+    transport: http(process.env.PAYMASTER_RPC),
+  });
+  const sudoKernelClient = createKernelAccountClient({
+    entryPoint,
+    account: sudoAccount,
+    chain: sepolia,
+    bundlerTransport: http(process.env.BUNDLER_RPC),
+    middleware: {
+      sponsorUserOperation: kernelPaymaster.sponsorUserOperation,
+    },
+  });
+
+  const emptyAccount = addressToEmptyAccount(sessionKeyAddress);
+  const emptySessionKeySigner = await toECDSASigner({ signer: emptyAccount });
+
+  const permissionPlugin = await toPermissionValidator(publicClient, {
+    entryPoint,
+    signer: emptySessionKeySigner,
+    policies: [
+      // In this example, we are just using a sudo policy to allow everything.
+      // In practice, you would want to set more restrictive policies.
+      toSudoPolicy({}),
+    ],
+  });
+
+  const unInstallTxHash = await sudoKernelClient.uninstallPlugin({
+    plugin: permissionPlugin,
+  });
+  console.log({ unInstallTxHash });
+};
 
 const main = async () => {
   // The agent creates a public-private key pair and sends
   // the public key (address) to the owner.
-  const sessionPrivateKey = generatePrivateKey()
-  const sessionKeyAccount = privateKeyToAccount(sessionPrivateKey)
+  const sessionPrivateKey = generatePrivateKey();
+  const sessionKeyAccount = privateKeyToAccount(sessionPrivateKey);
 
   const sessionKeySigner = await toECDSASigner({
     signer: sessionKeyAccount,
-  })
+  });
 
   // The owner approves the session key by signing its address and sending
   // back the signature
-  const approval = await getApproval(sessionKeySigner.account.address)
+  const approval = await getApproval(sessionKeySigner.account.address);
 
   // The agent constructs a full session key
-  await useSessionKey(approval, sessionKeySigner)
-}
+  await useSessionKey(approval, sessionKeySigner);
 
-main()
+  // revoke session key
+  await revokeSessionKey(sessionKeySigner.account.address);
+};
+
+main();
