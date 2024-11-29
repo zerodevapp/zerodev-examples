7a72f7a223ec812599b4f9a7eb5cb5a448640489
adnpark <aidenp.dev@gmail.com>
2024-11-29 21:12:24 +0900

    feat: update multi-chain example

--- multi-chain/main.ts
+++ multi-chain/main.ts
@@ -1,192 +1,203 @@
-import dotenv from "dotenv";
+import dotenv from "dotenv"
 import {
-  signUserOperations,
-  toMultiChainECDSAValidator,
-} from "@zerodev/multi-chain-ecdsa-validator";
+    sendUserOperations,
+    SendUserOperationsParameters,
+    signUserOperations,
+    toMultiChainECDSAValidator
+} from "@zerodev/multi-chain-ecdsa-validator"
 import {
-  createKernelAccount,
-  createKernelAccountClient,
-  createZeroDevPaymasterClient,
-} from "@zerodev/sdk";
-import { Hex, createPublicClient, http, zeroAddress } from "viem";
-import { privateKeyToAccount } from "viem/accounts";
-import { optimismSepolia, sepolia } from "viem/chains";
-import { getEntryPoint, KERNEL_V3_1 } from "@zerodev/sdk/constants";
+    createKernelAccount,
+    createKernelAccountClient,
+    createZeroDevPaymasterClient
+} from "@zerodev/sdk"
+import {
+    Chain,
+    Client,
+    Hex,
+    Transport,
+    createPublicClient,
+    http,
+    zeroAddress
+} from "viem"
+import { privateKeyToAccount } from "viem/accounts"
+import { optimismSepolia, sepolia } from "viem/chains"
+import { getEntryPoint, KERNEL_V3_1 } from "@zerodev/sdk/constants"
+import { SmartAccount } from "viem/account-abstraction"
 
-dotenv.config();
+dotenv.config()
 
 if (
-  !process.env.PRIVATE_KEY ||
-  !process.env.RPC_URL ||
-  !process.env.OPTIMISM_SEPOLIA_RPC_URL ||
-  !process.env.BUNDLER_RPC ||
-  !process.env.PAYMASTER_RPC ||
-  !process.env.OPTIMISM_SEPOLIA_BUNDLER_RPC_URL ||
-  !process.env.OPTIMISM_SEPOLIA_PAYMASTER_RPC_URL
+    !process.env.PRIVATE_KEY ||
+    !process.env.RPC_URL ||
+    !process.env.OPTIMISM_SEPOLIA_RPC_URL ||
+    !process.env.BUNDLER_RPC ||
+    !process.env.PAYMASTER_RPC ||
+    !process.env.OPTIMISM_SEPOLIA_BUNDLER_RPC_URL ||
+    !process.env.OPTIMISM_SEPOLIA_PAYMASTER_RPC_URL
 ) {
-  console.error(
-    "Please set PRIVATE_KEY, RPC_URL, OPTIMISM_SEPOLIA_RPC_URL, BUNDLER_RPC, PAYMASTER_RPC, OPTIMISM_SEPOLIA_BUNDLER_RPC_URL, OPTIMISM_SEPOLIA_PAYMASTER_RPC_URL"
-  );
-  process.exit(1);
+    console.error(
+        "Please set PRIVATE_KEY, RPC_URL, OPTIMISM_SEPOLIA_RPC_URL, BUNDLER_RPC, PAYMASTER_RPC, OPTIMISM_SEPOLIA_BUNDLER_RPC_URL, OPTIMISM_SEPOLIA_PAYMASTER_RPC_URL"
+    )
+    process.exit(1)
 }
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
-const SEPOLIA_RPC_URL = process.env.RPC_URL;
-const OPTIMISM_SEPOLIA_RPC_URL = process.env.OPTIMISM_SEPOLIA_RPC_URL;
+const SEPOLIA_RPC_URL = process.env.RPC_URL
+const OPTIMISM_SEPOLIA_RPC_URL = process.env.OPTIMISM_SEPOLIA_RPC_URL
 
-const SEPOLIA_ZERODEV_RPC_URL = process.env.BUNDLER_RPC;
-const SEPOLIA_ZERODEV_PAYMASTER_RPC_URL = process.env.PAYMASTER_RPC;
+const SEPOLIA_ZERODEV_RPC_URL = process.env.BUNDLER_RPC
+const SEPOLIA_ZERODEV_PAYMASTER_RPC_URL = process.env.PAYMASTER_RPC
 
 const OPTIMISM_SEPOLIA_ZERODEV_RPC_URL =
-  process.env.OPTIMISM_SEPOLIA_BUNDLER_RPC_URL;
+    process.env.OPTIMISM_SEPOLIA_BUNDLER_RPC_URL
 const OPTIMISM_SEPOLIA_ZERODEV_PAYMASTER_RPC_URL =
-  process.env.OPTIMISM_SEPOLIA_PAYMASTER_RPC_URL;
+    process.env.OPTIMISM_SEPOLIA_PAYMASTER_RPC_URL
 
-const entryPoint = getEntryPoint("0.7");
+const entryPoint = getEntryPoint("0.7")
 
 const main = async () => {
-  const sepoliaPublicClient = createPublicClient({
-    transport: http(SEPOLIA_RPC_URL),
-    chain: sepolia,
-  });
-  const optimismSepoliaPublicClient = createPublicClient({
-    transport: http(OPTIMISM_SEPOLIA_RPC_URL),
-    chain: optimismSepolia,
-  });
+    const sepoliaPublicClient = createPublicClient({
+        transport: http(SEPOLIA_RPC_URL),
+        chain: sepolia
+    })
+    const optimismSepoliaPublicClient = createPublicClient({
+        transport: http(OPTIMISM_SEPOLIA_RPC_URL),
+        chain: optimismSepolia
+    })
 
-  const signer = privateKeyToAccount(PRIVATE_KEY as Hex);
-  const sepoliaMultiSigECDSAValidatorPlugin = await toMultiChainECDSAValidator(
-    sepoliaPublicClient,
-    {
-      entryPoint,
-      signer,
-      kernelVersion: KERNEL_V3_1,
-      multiChainIds: [sepolia.id, optimismSepolia.id],
-    }
-  );
-  const optimismSepoliaMultiSigECDSAValidatorPlugin =
-    await toMultiChainECDSAValidator(optimismSepoliaPublicClient, {
-      entryPoint,
-      signer,
-      kernelVersion: KERNEL_V3_1,
-      multiChainIds: [sepolia.id, optimismSepolia.id],
-    });
+    const signer = privateKeyToAccount(PRIVATE_KEY as Hex)
+    const sepoliaMultiSigECDSAValidatorPlugin =
+        await toMultiChainECDSAValidator(sepoliaPublicClient, {
+            entryPoint,
+            signer,
+            kernelVersion: KERNEL_V3_1,
+            multiChainIds: [sepolia.id, optimismSepolia.id]
+        })
+    const optimismSepoliaMultiSigECDSAValidatorPlugin =
+        await toMultiChainECDSAValidator(optimismSepoliaPublicClient, {
+            entryPoint,
+            signer,
+            kernelVersion: KERNEL_V3_1,
+            multiChainIds: [sepolia.id, optimismSepolia.id]
+        })
 
-  const sepoliaKernelAccount = await createKernelAccount(sepoliaPublicClient, {
-    entryPoint,
-    plugins: {
-      sudo: sepoliaMultiSigECDSAValidatorPlugin,
-    },
-    kernelVersion: KERNEL_V3_1,
-  });
-
-  const optimismSepoliaKernelAccount = await createKernelAccount(
-    optimismSepoliaPublicClient,
-    {
-      entryPoint,
-      plugins: {
-        sudo: optimismSepoliaMultiSigECDSAValidatorPlugin,
-      },
-      kernelVersion: KERNEL_V3_1,
-    }
-  );
-
-  console.log("sepoliaKernelAccount.address", sepoliaKernelAccount.address);
-  console.log(
-    "optimismSepoliaKernelAccount.address",
-    optimismSepoliaKernelAccount.address
-  );
-
-  const sepoliaZeroDevPaymasterClient = createZeroDevPaymasterClient({
-    chain: sepolia,
-    transport: http(SEPOLIA_ZERODEV_PAYMASTER_RPC_URL),
-  });
-
-  const opSepoliaZeroDevPaymasterClient = createZeroDevPaymasterClient({
-    chain: optimismSepolia,
-    transport: http(OPTIMISM_SEPOLIA_ZERODEV_PAYMASTER_RPC_URL),
-  });
-
-  const sepoliaZerodevKernelClient = createKernelAccountClient({
-    account: sepoliaKernelAccount,
-    chain: sepolia,
-    bundlerTransport: http(SEPOLIA_ZERODEV_RPC_URL),
-    paymaster: {
-      getPaymasterData(userOperation) {
-        return sepoliaZeroDevPaymasterClient.sponsorUserOperation({
-          userOperation,
-        });
-      },
-    },
-  });
-
-  const optimismSepoliaZerodevKernelClient = createKernelAccountClient({
-    account: optimismSepoliaKernelAccount,
-    chain: optimismSepolia,
-    bundlerTransport: http(OPTIMISM_SEPOLIA_ZERODEV_RPC_URL),
-    paymaster: {
-      getPaymasterData(userOperation) {
-        return opSepoliaZeroDevPaymasterClient.sponsorUserOperation({
-          userOperation,
-        });
-      },
-    },
-  });
-
-  const sepoliaUserOp = await sepoliaZerodevKernelClient.prepareUserOperation({
-    callData: await sepoliaKernelAccount.encodeCalls([
-      {
-        to: zeroAddress,
-        value: BigInt(0),
-        data: "0x",
-      },
-    ]),
-  });
-
-  const optimismSepoliaUserOp =
-    await optimismSepoliaZerodevKernelClient.prepareUserOperation({
-      callData: await optimismSepoliaKernelAccount.encodeCalls([
+    const sepoliaKernelAccount = await createKernelAccount(
+        sepoliaPublicClient,
         {
-          to: zeroAddress,
-          value: BigInt(0),
-          data: "0x",
+            entryPoint,
+            plugins: {
+                sudo: sepoliaMultiSigECDSAValidatorPlugin
+            },
+            kernelVersion: KERNEL_V3_1
+        }
+    )
+
+    const optimismSepoliaKernelAccount = await createKernelAccount(
+        optimismSepoliaPublicClient,
+        {
+            entryPoint,
+            plugins: {
+                sudo: optimismSepoliaMultiSigECDSAValidatorPlugin
+            },
+            kernelVersion: KERNEL_V3_1
+        }
+    )
+
+    console.log("sepoliaKernelAccount.address", sepoliaKernelAccount.address)
+    console.log(
+        "optimismSepoliaKernelAccount.address",
+        optimismSepoliaKernelAccount.address
+    )
+
+    const sepoliaZeroDevPaymasterClient = createZeroDevPaymasterClient({
+        chain: sepolia,
+        transport: http(SEPOLIA_ZERODEV_PAYMASTER_RPC_URL)
+    })
+
+    const opSepoliaZeroDevPaymasterClient = createZeroDevPaymasterClient({
+        chain: optimismSepolia,
+        transport: http(OPTIMISM_SEPOLIA_ZERODEV_PAYMASTER_RPC_URL)
+    })
+
+    const sepoliaZerodevKernelClient = createKernelAccountClient({
+        account: sepoliaKernelAccount,
+        chain: sepolia,
+        bundlerTransport: http(SEPOLIA_ZERODEV_RPC_URL),
+        paymaster: {
+            getPaymasterData(userOperation) {
+                return sepoliaZeroDevPaymasterClient.sponsorUserOperation({
+                    userOperation
+                })
+            }
+        }
+    })
+
+    const optimismSepoliaZerodevKernelClient = createKernelAccountClient({
+        account: optimismSepoliaKernelAccount,
+        chain: optimismSepolia,
+        bundlerTransport: http(OPTIMISM_SEPOLIA_ZERODEV_RPC_URL),
+        paymaster: {
+            getPaymasterData(userOperation) {
+                return opSepoliaZeroDevPaymasterClient.sponsorUserOperation({
+                    userOperation
+                })
+            }
+        }
+    })
+
+    const clients: Client<Transport, Chain, SmartAccount>[] = [
+        {
+            ...sepoliaZerodevKernelClient
         },
-      ]),
-    });
+        {
+            ...optimismSepoliaZerodevKernelClient
+        }
+    ]
 
-  const signedUserOps = await signUserOperations(sepoliaZerodevKernelClient, {
-    account: sepoliaKernelAccount,
-    userOperations: [
-      { ...sepoliaUserOp, chainId: sepolia.id },
-      {
-        ...optimismSepoliaUserOp,
-        chainId: optimismSepolia.id,
-      },
-    ],
-  });
+    const userOps = await Promise.all(
+        clients.map(async (client) => {
+            return {
+                callData: await client.account.encodeCalls([
+                    {
+                        to: zeroAddress,
+                        value: BigInt(0),
+                        data: "0x"
+                    }
+                ])
+            }
+        })
+    )
 
-  console.log("sending sepoliaUserOp");
-  // you should use bundler client to send signed user ops
-  const sepoliaUserOpHash = await sepoliaZerodevKernelClient.sendUserOperation({
-    ...signedUserOps[0],
-  });
+    const userOpParams = [
+        {
+            ...userOps[0],
+            chainId: sepolia.id
+        },
+        {
+            ...userOps[1],
+            chainId: optimismSepolia.id
+        }
+    ]
 
-  console.log("sepoliaUserOpHash", sepoliaUserOpHash);
-  await sepoliaZerodevKernelClient.waitForUserOperationReceipt({
-    hash: sepoliaUserOpHash,
-  });
+    const userOpHashes = await sendUserOperations(clients, userOpParams)
+    const sepoliaUserOpHash = userOpHashes[0]
+    const optimismSepoliaUserOpHash = userOpHashes[1]
 
-  console.log("sending optimismSepoliaUserOp");
-  const optimismSepoliaUserOpHash =
-    await optimismSepoliaZerodevKernelClient.sendUserOperation({
-      ...signedUserOps[1],
-    });
+    console.log("sending sepoliaUserOp")
+    // you should use bundler client to send signed user ops
 
-  console.log("optimismSepoliaUserOpHash", optimismSepoliaUserOpHash);
-  await optimismSepoliaZerodevKernelClient.waitForUserOperationReceipt({
-    hash: optimismSepoliaUserOpHash,
-  });
-};
+    console.log("sepoliaUserOpHash", sepoliaUserOpHash)
+    await sepoliaZerodevKernelClient.waitForUserOperationReceipt({
+        hash: sepoliaUserOpHash
+    })
 
-main();
+    console.log("sending optimismSepoliaUserOp")
+
+    console.log("optimismSepoliaUserOpHash", optimismSepoliaUserOpHash)
+    await optimismSepoliaZerodevKernelClient.waitForUserOperationReceipt({
+        hash: optimismSepoliaUserOpHash
+    })
+}
+
+main()
--- multi-chain/sendUserOpsWithEnable.ts
+++ multi-chain/sendUserOpsWithEnable.ts
@@ -1,244 +1,248 @@
-import dotenv from "dotenv";
+import dotenv from "dotenv"
 import {
-  ecdsaSignUserOpsWithEnable,
-  toMultiChainECDSAValidator,
-} from "@zerodev/multi-chain-ecdsa-validator";
+    ecdsaSignUserOpsWithEnable,
+    sendUserOperations,
+    toMultiChainECDSAValidator
+} from "@zerodev/multi-chain-ecdsa-validator"
 import {
-  createKernelAccount,
-  createKernelAccountClient,
-  createZeroDevPaymasterClient,
-} from "@zerodev/sdk";
-import { Hex, createPublicClient, http, zeroAddress } from "viem";
-import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
-import { optimismSepolia, sepolia } from "viem/chains";
-import { toECDSASigner } from "@zerodev/permissions/signers";
-import { toSudoPolicy } from "@zerodev/permissions/policies";
-import { toPermissionValidator } from "@zerodev/permissions";
-import { getEntryPoint, KERNEL_V3_1 } from "@zerodev/sdk/constants";
+    createKernelAccount,
+    createKernelAccountClient,
+    createZeroDevPaymasterClient
+} from "@zerodev/sdk"
+import {
+    Chain,
+    Client,
+    Hex,
+    Transport,
+    createPublicClient,
+    http,
+    zeroAddress
+} from "viem"
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
+import { optimismSepolia, sepolia } from "viem/chains"
+import { toECDSASigner } from "@zerodev/permissions/signers"
+import { toSudoPolicy } from "@zerodev/permissions/policies"
+import { toPermissionValidator } from "@zerodev/permissions"
+import { getEntryPoint, KERNEL_V3_1 } from "@zerodev/sdk/constants"
+import { SmartAccount } from "viem/account-abstraction"
 
-dotenv.config();
+dotenv.config()
 
 if (
-  !process.env.PRIVATE_KEY ||
-  !process.env.RPC_URL ||
-  !process.env.OPTIMISM_SEPOLIA_RPC_URL ||
-  !process.env.BUNDLER_RPC ||
-  !process.env.PAYMASTER_RPC ||
-  !process.env.OPTIMISM_SEPOLIA_BUNDLER_RPC_URL ||
-  !process.env.OPTIMISM_SEPOLIA_PAYMASTER_RPC_URL
+    !process.env.PRIVATE_KEY ||
+    !process.env.RPC_URL ||
+    !process.env.OPTIMISM_SEPOLIA_RPC_URL ||
+    !process.env.BUNDLER_RPC ||
+    !process.env.PAYMASTER_RPC ||
+    !process.env.OPTIMISM_SEPOLIA_BUNDLER_RPC_URL ||
+    !process.env.OPTIMISM_SEPOLIA_PAYMASTER_RPC_URL
 ) {
-  console.error(
-    "Please set PRIVATE_KEY, RPC_URL, OPTIMISM_SEPOLIA_RPC_URL, BUNDLER_RPC, PAYMASTER_RPC, OPTIMISM_SEPOLIA_BUNDLER_RPC_URL, OPTIMISM_SEPOLIA_PAYMASTER_RPC_URL"
-  );
-  process.exit(1);
+    console.error(
+        "Please set PRIVATE_KEY, RPC_URL, OPTIMISM_SEPOLIA_RPC_URL, BUNDLER_RPC, PAYMASTER_RPC, OPTIMISM_SEPOLIA_BUNDLER_RPC_URL, OPTIMISM_SEPOLIA_PAYMASTER_RPC_URL"
+    )
+    process.exit(1)
 }
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
-const SEPOLIA_RPC_URL = process.env.RPC_URL;
-const OPTIMISM_SEPOLIA_RPC_URL = process.env.OPTIMISM_SEPOLIA_RPC_URL;
+const SEPOLIA_RPC_URL = process.env.RPC_URL
+const OPTIMISM_SEPOLIA_RPC_URL = process.env.OPTIMISM_SEPOLIA_RPC_URL
 
-const SEPOLIA_ZERODEV_RPC_URL = process.env.BUNDLER_RPC;
-const SEPOLIA_ZERODEV_PAYMASTER_RPC_URL = process.env.PAYMASTER_RPC;
+const SEPOLIA_ZERODEV_RPC_URL = process.env.BUNDLER_RPC
+const SEPOLIA_ZERODEV_PAYMASTER_RPC_URL = process.env.PAYMASTER_RPC
 
 const OPTIMISM_SEPOLIA_ZERODEV_RPC_URL =
-  process.env.OPTIMISM_SEPOLIA_BUNDLER_RPC_URL;
+    process.env.OPTIMISM_SEPOLIA_BUNDLER_RPC_URL
 const OPTIMISM_SEPOLIA_ZERODEV_PAYMASTER_RPC_URL =
-  process.env.OPTIMISM_SEPOLIA_PAYMASTER_RPC_URL;
+    process.env.OPTIMISM_SEPOLIA_PAYMASTER_RPC_URL
 
-const entryPoint = getEntryPoint("0.7");
+const entryPoint = getEntryPoint("0.7")
 
 const main = async () => {
-  const sepoliaPublicClient = createPublicClient({
-    transport: http(SEPOLIA_RPC_URL),
-    chain: sepolia,
-  });
-  const optimismSepoliaPublicClient = createPublicClient({
-    transport: http(OPTIMISM_SEPOLIA_RPC_URL),
-    chain: optimismSepolia,
-  });
+    const sepoliaPublicClient = createPublicClient({
+        transport: http(SEPOLIA_RPC_URL),
+        chain: sepolia
+    })
+    const optimismSepoliaPublicClient = createPublicClient({
+        transport: http(OPTIMISM_SEPOLIA_RPC_URL),
+        chain: optimismSepolia
+    })
 
-  const signer = privateKeyToAccount(PRIVATE_KEY as Hex);
-  const sepoliaMultiSigECDSAValidatorPlugin = await toMultiChainECDSAValidator(
-    sepoliaPublicClient,
-    {
-      entryPoint,
-      signer,
-      kernelVersion: KERNEL_V3_1,
-    }
-  );
-  const optimismSepoliaMultiSigECDSAValidatorPlugin =
-    await toMultiChainECDSAValidator(optimismSepoliaPublicClient, {
-      entryPoint,
-      signer,
-      kernelVersion: KERNEL_V3_1,
-    });
+    const signer = privateKeyToAccount(PRIVATE_KEY as Hex)
+    const sepoliaMultiSigECDSAValidatorPlugin =
+        await toMultiChainECDSAValidator(sepoliaPublicClient, {
+            entryPoint,
+            signer,
+            kernelVersion: KERNEL_V3_1
+        })
+    const optimismSepoliaMultiSigECDSAValidatorPlugin =
+        await toMultiChainECDSAValidator(optimismSepoliaPublicClient, {
+            entryPoint,
+            signer,
+            kernelVersion: KERNEL_V3_1
+        })
 
-  const sepoliaEcdsaSigner = privateKeyToAccount(generatePrivateKey());
-  const sepoliaEcdsaModularSigner = await toECDSASigner({
-    signer: sepoliaEcdsaSigner,
-  });
+    const sepoliaEcdsaSigner = privateKeyToAccount(generatePrivateKey())
+    const sepoliaEcdsaModularSigner = await toECDSASigner({
+        signer: sepoliaEcdsaSigner
+    })
 
-  const optimismSepoliaEcdsaSigner = privateKeyToAccount(generatePrivateKey());
-  const optimismSepoliaEcdsaModularSigner = await toECDSASigner({
-    signer: optimismSepoliaEcdsaSigner,
-  });
+    const optimismSepoliaEcdsaSigner = privateKeyToAccount(generatePrivateKey())
+    const optimismSepoliaEcdsaModularSigner = await toECDSASigner({
+        signer: optimismSepoliaEcdsaSigner
+    })
 
-  const sudoPolicy = toSudoPolicy({});
+    const sudoPolicy = toSudoPolicy({})
 
-  const sepoliaPermissionPlugin = await toPermissionValidator(
-    sepoliaPublicClient,
-    {
-      entryPoint,
-      signer: sepoliaEcdsaModularSigner,
-      policies: [sudoPolicy],
-      kernelVersion: KERNEL_V3_1,
-    }
-  );
+    const sepoliaPermissionPlugin = await toPermissionValidator(
+        sepoliaPublicClient,
+        {
+            entryPoint,
+            signer: sepoliaEcdsaModularSigner,
+            policies: [sudoPolicy],
+            kernelVersion: KERNEL_V3_1
+        }
+    )
 
-  const optimismSepoliaPermissionPlugin = await toPermissionValidator(
-    optimismSepoliaPublicClient,
-    {
-      entryPoint,
-      signer: optimismSepoliaEcdsaModularSigner,
-      policies: [sudoPolicy],
-      kernelVersion: KERNEL_V3_1,
-    }
-  );
+    const optimismSepoliaPermissionPlugin = await toPermissionValidator(
+        optimismSepoliaPublicClient,
+        {
+            entryPoint,
+            signer: optimismSepoliaEcdsaModularSigner,
+            policies: [sudoPolicy],
+            kernelVersion: KERNEL_V3_1
+        }
+    )
 
-  const sepoliaKernelAccount = await createKernelAccount(sepoliaPublicClient, {
-    entryPoint,
-    plugins: {
-      sudo: sepoliaMultiSigECDSAValidatorPlugin,
-      regular: sepoliaPermissionPlugin,
-    },
-    kernelVersion: KERNEL_V3_1,
-  });
+    const sepoliaKernelAccount = await createKernelAccount(
+        sepoliaPublicClient,
+        {
+            entryPoint,
+            plugins: {
+                sudo: sepoliaMultiSigECDSAValidatorPlugin,
+                regular: sepoliaPermissionPlugin
+            },
+            kernelVersion: KERNEL_V3_1
+        }
+    )
 
-  const optimismSepoliaKernelAccount = await createKernelAccount(
-    optimismSepoliaPublicClient,
-    {
-      entryPoint,
-      plugins: {
-        sudo: optimismSepoliaMultiSigECDSAValidatorPlugin,
-        regular: optimismSepoliaPermissionPlugin,
-      },
-      kernelVersion: KERNEL_V3_1,
-    }
-  );
+    const optimismSepoliaKernelAccount = await createKernelAccount(
+        optimismSepoliaPublicClient,
+        {
+            entryPoint,
+            plugins: {
+                sudo: optimismSepoliaMultiSigECDSAValidatorPlugin,
+                regular: optimismSepoliaPermissionPlugin
+            },
+            kernelVersion: KERNEL_V3_1
+        }
+    )
 
-  console.log("sepoliaKernelAccount.address", sepoliaKernelAccount.address);
-  console.log(
-    "optimismSepoliaKernelAccount.address",
-    optimismSepoliaKernelAccount.address
-  );
+    console.log("sepoliaKernelAccount.address", sepoliaKernelAccount.address)
+    console.log(
+        "optimismSepoliaKernelAccount.address",
+        optimismSepoliaKernelAccount.address
+    )
 
-  const sepoliaZeroDevPaymasterClient = createZeroDevPaymasterClient({
-    chain: sepolia,
-    transport: http(SEPOLIA_ZERODEV_PAYMASTER_RPC_URL),
-  });
+    const sepoliaZeroDevPaymasterClient = createZeroDevPaymasterClient({
+        chain: sepolia,
+        transport: http(SEPOLIA_ZERODEV_PAYMASTER_RPC_URL)
+    })
 
-  const opSepoliaZeroDevPaymasterClient = createZeroDevPaymasterClient({
-    chain: optimismSepolia,
-    transport: http(OPTIMISM_SEPOLIA_ZERODEV_PAYMASTER_RPC_URL),
-  });
+    const opSepoliaZeroDevPaymasterClient = createZeroDevPaymasterClient({
+        chain: optimismSepolia,
+        transport: http(OPTIMISM_SEPOLIA_ZERODEV_PAYMASTER_RPC_URL)
+    })
 
-  const sepoliaZerodevKernelClient = createKernelAccountClient({
-    account: sepoliaKernelAccount,
-    chain: sepolia,
-    bundlerTransport: http(SEPOLIA_ZERODEV_RPC_URL),
-    paymaster: {
-      getPaymasterData(userOperation) {
-        return sepoliaZeroDevPaymasterClient.sponsorUserOperation({
-          userOperation,
-        });
-      },
-    },
-  });
+    const sepoliaZerodevKernelClient = createKernelAccountClient({
+        account: sepoliaKernelAccount,
+        chain: sepolia,
+        bundlerTransport: http(SEPOLIA_ZERODEV_RPC_URL),
+        paymaster: {
+            getPaymasterData(userOperation) {
+                return sepoliaZeroDevPaymasterClient.sponsorUserOperation({
+                    userOperation
+                })
+            }
+        }
+    })
 
-  const optimismSepoliaZerodevKernelClient = createKernelAccountClient({
-    account: optimismSepoliaKernelAccount,
-    chain: optimismSepolia,
-    bundlerTransport: http(OPTIMISM_SEPOLIA_ZERODEV_RPC_URL),
-    paymaster: {
-      getPaymasterData(userOperation) {
-        return opSepoliaZeroDevPaymasterClient.sponsorUserOperation({
-          userOperation,
-        });
-      },
-    },
-  });
+    const optimismSepoliaZerodevKernelClient = createKernelAccountClient({
+        account: optimismSepoliaKernelAccount,
+        chain: optimismSepolia,
+        bundlerTransport: http(OPTIMISM_SEPOLIA_ZERODEV_RPC_URL),
+        paymaster: {
+            getPaymasterData(userOperation) {
+                return opSepoliaZeroDevPaymasterClient.sponsorUserOperation({
+                    userOperation
+                })
+            }
+        }
+    })
 
-  const sepoliaUserOp = await sepoliaZerodevKernelClient.prepareUserOperation({
-    callData: await sepoliaKernelAccount.encodeCalls([
-      {
+    const clients: Client<Transport, Chain, SmartAccount>[] = [
+        {
+            ...sepoliaZerodevKernelClient
+        },
+        {
+            ...optimismSepoliaZerodevKernelClient
+        }
+    ]
+
+    const userOps = await Promise.all(
+        clients.map(async (client) => {
+            return {
+                callData: await client.account.encodeCalls([
+                    {
+                        to: zeroAddress,
+                        value: BigInt(0),
+                        data: "0x"
+                    }
+                ])
+            }
+        })
+    )
+
+    const userOpParams = [
+        {
+            ...userOps[0],
+            chainId: sepolia.id
+        },
+        {
+            ...userOps[1],
+            chainId: optimismSepolia.id
+        }
+    ]
+
+    const userOpHashes = await sendUserOperations(clients, userOpParams)
+    const sepoliaUserOpHash = userOpHashes[0]
+    const optimismSepoliaUserOpHash = userOpHashes[1]
+
+    console.log("sepoliaUserOpHash", sepoliaUserOpHash)
+    await sepoliaZerodevKernelClient.waitForUserOperationReceipt({
+        hash: sepoliaUserOpHash
+    })
+
+    console.log("optimismSepoliaUserOpHash", optimismSepoliaUserOpHash)
+    await optimismSepoliaZerodevKernelClient.waitForUserOperationReceipt({
+        hash: optimismSepoliaUserOpHash
+    })
+
+    // now you can use sendTransaction or sendUserOperation since you've already enabled the regular validator, which is permission here.
+    const sepoliaTxHash = await sepoliaZerodevKernelClient.sendTransaction({
         to: zeroAddress,
         value: BigInt(0),
-        data: "0x",
-      },
-    ]),
-  });
+        data: "0x"
+    })
+    console.log("sepoliaTxHash", sepoliaTxHash)
 
-  const optimismSepoliaUserOp =
-    await optimismSepoliaZerodevKernelClient.prepareUserOperation({
-      callData: await optimismSepoliaKernelAccount.encodeCalls([
-        {
-          to: zeroAddress,
-          value: BigInt(0),
-          data: "0x",
-        },
-      ]),
-    });
+    const optimismSepoliaTxHash =
+        await optimismSepoliaZerodevKernelClient.sendTransaction({
+            to: zeroAddress,
+            value: BigInt(0),
+            data: "0x"
+        })
+    console.log("optimismSepoliaTxHash", optimismSepoliaTxHash)
+}
 
-  // signUserOpsWithEnable will configure the signature as a combination of enable signatures and actual user operation signatures.
-  const signedUserOps = await ecdsaSignUserOpsWithEnable({
-    multiChainUserOpConfigsForEnable: [
-      {
-        account: sepoliaKernelAccount,
-        userOp: sepoliaUserOp,
-      },
-      {
-        account: optimismSepoliaKernelAccount,
-        userOp: optimismSepoliaUserOp,
-      },
-    ],
-  });
-
-  // You should send the signed user operations to enable the regular validator with bundler client.
-  const sepoliaUserOpHash = await sepoliaZerodevKernelClient.sendUserOperation({
-    ...signedUserOps[0],
-  });
-
-  console.log("sepoliaUserOpHash", sepoliaUserOpHash);
-  await sepoliaZerodevKernelClient.waitForUserOperationReceipt({
-    hash: sepoliaUserOpHash,
-  });
-
-  const optimismSepoliaUserOpHash =
-    await optimismSepoliaZerodevKernelClient.sendUserOperation({
-      ...signedUserOps[1],
-    });
-
-  console.log("optimismSepoliaUserOpHash", optimismSepoliaUserOpHash);
-  await optimismSepoliaZerodevKernelClient.waitForUserOperationReceipt({
-    hash: optimismSepoliaUserOpHash,
-  });
-
-  // now you can use sendTransaction or sendUserOperation since you've already enabled the regular validator, which is permission here.
-  const sepoliaTxHash = await sepoliaZerodevKernelClient.sendTransaction({
-    to: zeroAddress,
-    value: BigInt(0),
-    data: "0x",
-  });
-  console.log("sepoliaTxHash", sepoliaTxHash);
-
-  const optimismSepoliaTxHash =
-    await optimismSepoliaZerodevKernelClient.sendTransaction({
-      to: zeroAddress,
-      value: BigInt(0),
-      data: "0x",
-    });
-  console.log("optimismSepoliaTxHash", optimismSepoliaTxHash);
-};
-
-main();
+main()
