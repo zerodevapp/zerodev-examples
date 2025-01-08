28cb1bdd57763e5ff70ecdb40c120cb95b2fe23d
jstinhw <justin84602@gmail.com>
2025-01-09 01:24:22 +0800

    fix: sponsored flow

--- intent/same-chain-v2.ts
+++ intent/same-chain-v2.ts
@@ -8,26 +8,22 @@ import {
   type Chain,
   createPublicClient,
   http,
-  encodeFunctionData,
-  concatHex,
   zeroAddress,
-  encodeAbiParameters,
-  parseAbiParameters
 } from "viem";
-import { createKernelAccount, KernelV3_1AccountAbi } from "@zerodev/sdk";
+import { createKernelAccount } from "@zerodev/sdk";
 import { privateKeyToAccount } from "viem/accounts";
 import { createIntentClient, installIntentExecutor, INTENT_V0_2 } from "@zerodev/intent";
-import { arbitrumSepolia, baseSepolia } from "viem/chains";
+import { base } from "viem/chains";
 
-if (!process.env.PRIVATE_KEY) {
-  throw new Error("PRIVATE_KEY is not set");
+if (!process.env.PRIVATE_KEY || !process.env.BASE_PROJECT_ID) {
+  throw new Error("PRIVATE_KEY or BASE_PROJECT_ID is not set");
 }
 
 const timeout = 100_000;
 const privateKey = process.env.PRIVATE_KEY as Hex;
 const account = privateKeyToAccount(privateKey);
 
-const chain = baseSepolia;
+const chain = base;
 const bundlerRpc = process.env.BUNDLER_RPC as string;
 
 const publicClient = createPublicClient({
@@ -47,7 +43,6 @@ async function createIntentClinet(chain: Chain) {
   // set kernel and entryPoint version
   const entryPoint = getEntryPoint("0.7");
   const kernelVersion = KERNEL_V3_2;
-  const intentExecutor = '0xAd8da92Dd670871bD3f90475d6763d520728881a';
 
   // create ecdsa validator
   const ecdsaValidator = await signerToEcdsaValidator(publicClient, {
@@ -62,20 +57,7 @@ async function createIntentClinet(chain: Chain) {
     },
     kernelVersion,
     entryPoint,
-    initConfig: [
-      encodeFunctionData({
-        abi: KernelV3_1AccountAbi,
-        functionName: "installModule",
-        args: [
-          BigInt(2),
-          intentExecutor,
-          concatHex([
-            zeroAddress,
-            encodeAbiParameters(parseAbiParameters(["bytes", "bytes"]), ["0x", "0x"]),
-          ]),
-        ],
-      })
-    ],
+    initConfig: [installIntentExecutor(INTENT_V0_2)],
   });
 
   // the cabclient can be used to send normal userOp and cross-chain cab tx
@@ -84,8 +66,8 @@ async function createIntentClinet(chain: Chain) {
     chain,
     bundlerTransport: http(bundlerRpc, { timeout }),
     version: INTENT_V0_2,
-    relayerTransport: http('https://relayer-testnet.onrender.com', { timeout }),
-    intentTransport: http('https://user-intent-service.onrender.com/intent', { timeout }),
+    relayerTransport: http(`http://127.0.0.1:8080/${process.env.BASE_PROJECT_ID}`, { timeout }),
+    intentTransport: http('http://127.0.0.1:3000/intent', { timeout }),
   });
 
   return intentClient;
@@ -94,27 +76,27 @@ async function createIntentClinet(chain: Chain) {
 async function main() {
   const intentClient = await createIntentClinet(chain);
 
-  while (true) {
-    console.log(
-      `Please deposit USDC to ${intentClient.account.address} on Arbitrum.`
-    );
-    await waitForUserInput();
-    const balance = await publicClient.readContract({
-      address: "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
-      abi: erc20Abi,
-      functionName: "balanceOf",
-      args: [intentClient.account.address],
-    });
-    if (balance >= parseUnits("0.01", 6)) {
-      break;
-    }
-    console.log(
-      `Insufficient USDC balance: ${formatUnits(
-        balance,
-        6
-      )}. Please deposit at least 0.1 USDC.`
-    );
-  }
+  // while (true) {
+  //   console.log(
+  //     `Please deposit USDC to ${intentClient.account.address} on Arbitrum.`
+  //   );
+  //   await waitForUserInput();
+  //   const balance = await publicClient.readContract({
+  //     address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+  //     abi: erc20Abi,
+  //     functionName: "balanceOf",
+  //     args: [intentClient.account.address],
+  //   });
+  //   if (balance >= parseUnits("0.1", 6)) {
+  //     break;
+  //   }
+  //   console.log(
+  //     `Insufficient USDC balance: ${formatUnits(
+  //       balance,
+  //       6
+  //     )}. Please deposit at least 0.1 USDC.`
+  //   );
+  // }
 
   // send the intent
   console.log("start sending UserIntent");
@@ -126,10 +108,10 @@ async function main() {
         data: "0x",
       }, 
     ],
-    gasToken: {
-      chainId: chain.id,
-      address: "0x036CbD53842c5426634e7929541eC2318f3dCF7e", // USDC on arb
-    },
+    // gasToken: {
+    //   chainId: chain.id,
+    //   address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913", // USDC on arb
+    // },
     chainId: chain.id,
   });
   console.log(`succesfully send cab tx, intentId: ${result.uiHash}`);
