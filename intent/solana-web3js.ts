d641ae80e25aeeb890c29885d44009e3a23afca1
KONFeature <quentin@nivelais.com>
2025-03-21 11:19:11 +0100

    feat: example with `@solana/web3js`

--- intent/solana-web3js.ts
+++ intent/solana-web3js.ts
@@ -1,21 +1,7 @@
 import * as dotenv from "dotenv"
 import { Chain, createPublicClient, http, formatUnits, parseUnits, type Hex, type ByteArray } from "viem"
 import { base } from "viem/chains"
-import { 
-  address, 
-  getBase58Encoder, 
-  createKeyPairSignerFromBytes, 
-  type Address as SolanaAddress, 
-  createSolanaRpc, 
-  getTransactionDecoder, 
-  getBase64EncodedWireTransaction, 
-  setTransactionMessageFeePayer, 
-  getCompiledTransactionMessageEncoder, 
-  compileTransactionMessage, 
-  appendTransactionMessageInstructions, 
-  setTransactionMessageLifetimeUsingBlockhash, 
-  createTransactionMessage, 
-  pipe } from "@solana/kit"
+import { address, getBase58Encoder, type Base64EncodedWireTransaction, type Address as SolanaAddress } from "@solana/kit"
 import {
   findAssociatedTokenPda,
   getCreateAssociatedTokenIdempotentInstructionAsync,
@@ -28,6 +14,8 @@ import { getEntryPoint } from "@zerodev/sdk/constants"
 import { toMultiChainECDSAValidator } from "@zerodev/multi-chain-ecdsa-validator"
 import { createKernelAccount } from "@zerodev/sdk"
 import { privateKeyToAccount } from "viem/accounts"
+import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js"
+import { createAssociatedTokenAccountIdempotentInstruction, createTransferInstruction, getOrCreateAssociatedTokenAccount } from "@solana/spl-token"
 
 dotenv.config()
 
@@ -49,8 +37,8 @@ const relayerRpc = process.env.RELAYER_RPC as string;
 // evm signer account
 const eoaAccount = privateKeyToAccount(evmPrivateKey);
 
-// New solana/kit signer
-const solanaSignerAsync = createKeyPairSignerFromBytes(solanaPrivateKey)
+// Legacy solana signer
+const solanaSignerLegacy = Keypair.fromSecretKey(solanaPrivateKey as Uint8Array)
 
 const publicClient = createPublicClient({
   chain,
@@ -65,10 +53,8 @@ const waitForUserInput = async () => {
   });
 };
 
-/**
- * Build the zerodev intent client with a `solana` extension
- */
 async function createIntentClinet(chain: Chain) {
+
   // set kernel and entryPoint version
   const entryPoint = getEntryPoint("0.7");
   const kernelVersion = KERNEL_V3_2;
@@ -90,10 +76,8 @@ async function createIntentClinet(chain: Chain) {
     initConfig: [installIntentExecutor(INTENT_V0_3)],
   });
 
-  const solanaSigner = await solanaSignerAsync;
-
   // the cab client can be used to send normal userOp and cross-chain cab tx
-  return createIntentClient({
+  const intentClient = createIntentClient({
     account: kernelAccount,
     chain,
     bundlerTransport: http(bundlerRpc, { timeout }),
@@ -101,27 +85,27 @@ async function createIntentClinet(chain: Chain) {
     // beta only
     relayerTransport: http(relayerRpc, { timeout }),
     intentTransport: http(intentRpc, { timeout }),
-    // solana specific parameters
+    // solana tx adapter
     solana: {
-      address: solanaSigner.address,
       rpcUrl: 'https://api.mainnet-beta.solana.com',
-      signTransaction: async (transaction: ByteArray) => {
-        const decoded = getTransactionDecoder().decode(transaction)
-        const [signatures] = await solanaSigner.signTransactions([decoded])
-        const newTransaction = {
-          ...decoded,
-          signatures: Object.freeze({
-            ...decoded.signatures,
-            ...signatures
-          }),
-        }
-        return getBase64EncodedWireTransaction(newTransaction);
+      address: solanaSignerLegacy.publicKey.toBase58() as unknown as SolanaAddress,
+      signTransaction: async (rawTransaction: ByteArray) => {
+        // Map the transaction to the solana transaction format and sign it
+        const transaction = Transaction.from(rawTransaction);
+        transaction.sign(solanaSignerLegacy)
+
+        // serialize the transaction and return the base64 encoded string 
+        //  todo: here some config could be pass to skip some checks
+        const buffer = transaction.serialize();
+        return Buffer.from(buffer).toString('base64') as Base64EncodedWireTransaction;
       },
     },
   });
+  return intentClient;
 }
 
 // transfer usdc to recipient
+// todo: missing the instruction for the `getCreateAssociatedTokenIdempotentInstructionAsync`
 const getInstructions = async ({
   token,
   amount,
@@ -133,49 +117,35 @@ const getInstructions = async ({
   decimal: number,
   recipient: SolanaAddress,
 }) => {
-  // New solana/kit signer
-  const solanaSigner = await solanaSignerAsync;
+  const connection = new Connection("https://api.mainnet-beta.solana.com")
 
-  // construct instructions
-  const [sourceAssociatedTokenAddress] = await findAssociatedTokenPda({
-    mint: token,
-    owner: solanaSigner.address,
-    tokenProgram: TOKEN_PROGRAM_ADDRESS,
-  });
-  const [destinationAssociatedTokenAddress] = await findAssociatedTokenPda({
-    mint: token,
-    owner: recipient,
-    tokenProgram: TOKEN_PROGRAM_ADDRESS,
-  });
-
-  const createAssociatedTokenInstruction = await getCreateAssociatedTokenIdempotentInstructionAsync(
-    {
-      mint: token,
-      owner: recipient,
-      payer: solanaSigner,
-    },
+  // Get the src and dst account
+  const sourceAccount = await getOrCreateAssociatedTokenAccount(
+    connection, 
+    solanaSignerLegacy,
+    new PublicKey(token),
+    solanaSignerLegacy.publicKey
   );
-  const transferInstruction = getTransferCheckedInstruction(
-    {
-      source: sourceAssociatedTokenAddress,
-      mint: token,
-      destination: destinationAssociatedTokenAddress,
-      authority: solanaSigner.address,
-      amount: amount,
-      decimals: decimal,
-    },
-    {
-      programAddress: TOKEN_PROGRAM_ADDRESS,
-    },
+  const destinationAccount = await getOrCreateAssociatedTokenAccount(
+    connection, 
+    solanaSignerLegacy,
+    new PublicKey(token),
+    new PublicKey(recipient)
   );
 
-  const instructions = [createAssociatedTokenInstruction, transferInstruction]
-  return instructions
+  // Create the transfer instruction
+  const transferInstruction = createTransferInstruction(
+    sourceAccount.address,
+    destinationAccount.address,
+    solanaSignerLegacy.publicKey,
+    amount
+  );
+
+  return [transferInstruction];
 }
 
 async function main() {
   const intentClient = await createIntentClinet(chain)
-  const solanaSigner = await solanaSignerAsync;
 
   // output token and chainId
   const decimal = 6;
@@ -190,7 +160,13 @@ async function main() {
     amount: outputAmount,
     decimal: decimal,
     recipient: address(solanaRecipient),
-  });
+  })
+
+  // Build the transacton
+  const transaction = new Transaction();
+  for (const instruction of instructions) {
+    transaction.add(instruction);
+  }
 
   // get cab to make sure the balance is enough
   while (true) {
@@ -214,27 +190,18 @@ async function main() {
     );
   }
 
-  // Prepare the transaction we want to execute with the intent client
-  const solanaRpc = createSolanaRpc('https://api.mainnet-beta.solana.com')
-  const { value: latestBlockhash } = await solanaRpc
-    .getLatestBlockhash()
-    .send();
-  const transactionMessage = pipe(
-    createTransactionMessage({ version: 0 }),
-    (message) =>
-      setTransactionMessageFeePayer(solanaSigner.address, message),
-    (message) =>
-      setTransactionMessageLifetimeUsingBlockhash(latestBlockhash, message),
-    (message) =>
-      appendTransactionMessageInstructions(instructions, message),
-  );
-  const compiledTransactionMessage =
-    compileTransactionMessage(transactionMessage);
-  const encodedTransactionMessage =
-    getCompiledTransactionMessageEncoder().encode(
-      compiledTransactionMessage,
-    );
+  // Add the block infos to the transaction
+  const connection = new Connection("https://api.mainnet-beta.solana.com")
+  const latestBlockhash = await connection.getLatestBlockhash("confirmed");
+  transaction.recentBlockhash = latestBlockhash.blockhash;
 
+  // todo: it have a check on the fee payer, so we put a placeholder one that will be replaced by the actual fee payer
+  transaction.feePayer = solanaSignerLegacy.publicKey;
+
+  // Serialize the transaction (we don't sign it yet since we will add fee payer and stuff)
+  transaction.verifySignatures(false)
+  const buffer = transaction.compileMessage().serialize();
+  const encodedTransaction = new Uint8Array(buffer);
 
   // send evm -> solana intent
   console.log("start sending intent evm -> solana");
@@ -249,22 +216,23 @@ async function main() {
       {
         chainId: outputChainId,
         address: outputToken,
-        amount: outputAmount,
+        amount: parseUnits("2.05", decimal),
       },
     ],
-    solTransaction: encodedTransactionMessage,
+    solTransaction: encodedTransaction,
   });
-  console.log(`successfully send cab tx, intentId: ${result.outputUiHash.uiHash}`);
+  console.log(`succesfully send cab tx, intentId: ${result.outputUiHash.uiHash}`);
+ 
+  // todo: e2e fcked up with payment token on relayer side
+  
 
   // wait for the intent to be opened on the input chains
   await Promise.all(
     result.inputsUiHash.map(async (data) => {
-      const openReceipts = await intentClient.waitForUserIntentOpenReceipt({
+      const openReceipts =  await intentClient.waitForUserIntentOpenReceipt({
         uiHash: data.uiHash,
       });
-      if (openReceipts?.receipt?.transactionHash) {
-        console.log(`intent open on chain ${openReceipts?.openChainId} txHash: ${openReceipts?.receipt?.transactionHash}`);
-      }
+      console.log(`intent open on chain ${openReceipts?.openChainId} txHash: ${openReceipts?.receipt?.transactionHash}`);
     })
   );
 
